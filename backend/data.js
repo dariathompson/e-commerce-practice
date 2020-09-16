fe5faeba52c74d31379ff904ef13e0ff69c7dd5c
Daria <daria.kambalina@gmail.com>
2020-09-16 19:25:51 +0100

    Created server

---
+++ backend/data.js
@@ -0,0 +1,32 @@
+export default {
+    products:[{
+        _id: '1',
+        name: 'Ball dog toy',
+        category: 'Toys',
+        image: '/images/d1.jpg',
+        price: 6,
+        brand: 'Toyz Space',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Chewing toy',
+        category: 'Toys',
+        image: '/images/d1.jpg',
+        price: 6,
+        brand: 'Toyz Space',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '3',
+        name: 'Chicken fillets',
+        category: 'Treats',
+        image: '/images/d3.jpg',
+        price: 8,
+        brand: 'Rosewood',
+        rating: 4.8,
+        numReviews: 4
+    }]
+}
