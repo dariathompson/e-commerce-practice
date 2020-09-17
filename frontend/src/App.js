18e4507d87589048ca7c002585776ac2e050a0c0
Daria <daria.kambalina@gmail.com>
2020-09-17 15:03:12 +0100

    Added redux for cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
 import './App.css';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
@@ -39,6 +40,7 @@ function App() {
       <main className="main">
         <div className="content">
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' exact={true} component={HomeScreen} />
         </div>
       </main>
