3b6e26ff627b985524084512a3e629d4eadccef3
Daria <daria.kambalina@gmail.com>
2020-09-17 13:48:03 +0100

    Added redux to product page

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,60 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen(props) {
-  console.log(props.match.params.id);
-  const product = data.products.find(x => x._id === props.match.params.id)
+
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(props.match.params.id);
+    dispatch(detailsProduct(props.match.params.id));
+  }, [])
+
   return(
     <div>
       <div className="back-to-result">
         <Link to='/'>Back to result</Link>
       </div>
-      <div className='details'>
-        <div className="details-image">
-          <img src={product.image} alt="product"></img>
+      { loading ? <div>Loading...</div> :
+        error ? <div>{error}</div> :
+        (
+          <div className='details'>
+          <div className="details-image">
+            <img src={product.image} alt="product"></img>
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.brand}
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </li>
+              <li>
+                Price: <b>${product.price}</b>
+              </li>
+              <li>
+                Description: {product.description}
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>
+                Price: {product.price}
+              </li>
+              <li>
+                Status: {product.status}
+              </li>
+              <li>
+                Qty: <select>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                </select>
+              </li>
+              <li>
+                <button className="cart-button">Add to cart</button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.brand}
-            </li>
-            <li>
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </li>
-            <li>
-              Price: <b>${product.price}</b>
-            </li>
-            <li>
-              Description: {product.description}
-            </li>
-          </ul>
-        </div>
-        <div className="details-action">
-          <ul>
-            <li>
-              Price: {product.price}
-            </li>
-            <li>
-              Status: {product.status}
-            </li>
-            <li>
-              Qty: <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-              </select>
-            </li>
-            <li>
-              <button className="cart-button">Add to cart</button>
-            </li>
-          </ul>
-        </div>
-      </div>
+        )
+      }
+
     </div>
         
   )
